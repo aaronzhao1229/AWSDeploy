422e8f53a2f8288023bf5fc0a78ce1cc0a59d2e6
赵建 <Z@MacBook-Pro.local>
2023-09-30 13:51:04 +1300

    change to specific

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,24 +3,24 @@
 module.exports = {
   development: {
     client: 'mysql2',
-    // connection: {
-    //   host: 'localhost',
-    //   user: 'root',
-    //   password: 'test1234!',
-    //   database: 'test_aws',
-    // },
+    connection: {
+      host: 'localhost',
+      user: 'aaronlaptop',
+      password: 'Test1234!',
+      database: 'test',
+    },
     // connection: {
     //   host: '13.210.13.136',
     //   user: 'aaronlaptop',
     //   password: 'Test1234!',
     //   database: 'test',
     // },
-    connection: {
-      host: '3.26.160.105',
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-    },
+    // connection: {
+    //   host: '3.26.160.105',
+    //   user: process.env.MYSQL_USER,
+    //   password: process.env.MYSQL_PASSWORD,
+    //   database: process.env.MYSQL_DATABASE,
+    // },
     useNullAsDefault: true,
   },
 
